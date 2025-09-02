508612bea211e856796b163e3533fcca921ddb85
raziel.rodrigues <raziel.rodrigues@bedrockstreaming.com>
2025-09-02 20:56:57 +0100

    feat: ok move ahead

---
+++ src/GraphQL/Mutation/CharacterMutation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\GraphQL\Mutation;
+
+use App\Entity\Character;
+use Doctrine\ORM\EntityManagerInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
+
+class CharacterMutation implements MutationInterface, AliasedInterface
+{
+
+    public function __construct(private EntityManagerInterface $entityManagerInterface) {
+     }
+
+    public function addCharacter(string $name) : array
+    {
+        return [12121];
+        $character = new Character();
+        $character->setName($name);
+        $this->entityManagerInterface->persist($character);
+        $this->entityManagerInterface->flush();
+     }
+
+    
+     public static function getAliases(): array
+     {
+        return [
+            'addCharacter' => 'add_character'
+        ];
+     }
+
+
+}
